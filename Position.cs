5195520c02bf56e7964483aa734f3bf378f244c4
Younes-ch <younes.jmell10@gmail.com>
2025-01-01 20:27:24 +0100

    Add a position class.

---
+++ Position.cs
@@ -0,0 +1,40 @@
+﻿namespace SnakeGame;
+
+public class Position
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+
+    public Position(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public Position Translate(Direction dir)
+    {
+        return new Position(Row + dir.RowOffset, Column + dir.ColumnOffset);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Row, Column);
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        return obj is Position pos
+            && Row == pos.Row
+            && Column == pos.Column;
+    }
+
+    public static bool operator ==(Position left, Position right)
+    {
+        return EqualityComparer<Position>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(Position left, Position right)
+    {
+        return !(left == right);
+    }
+}
