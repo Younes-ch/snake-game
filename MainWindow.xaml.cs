0cdfdc8a7c56110f4f57b0b71b8a1724930f6f20
Younes-ch <younes.jmell10@gmail.com>
2025-01-01 23:26:59 +0100

    Add the UI and drawing the grid.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,8 +16,35 @@ namespace SnakeGame;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly int _rows = 15, _cols = 15;
+    private readonly Image[,] gridImages;
+    
     public MainWindow()
     {
         InitializeComponent();
+        gridImages = SetupGrid();
+    }
+
+    private Image[,] SetupGrid()
+    {
+        var images = new Image[_rows, _cols];
+        GameGrid.Rows = _rows;
+        GameGrid.Columns = _cols;
+
+        for (var r = 0; r < _rows; r++)
+        {
+            for (var c = 0; c < _cols; c++)
+            {
+                var image = new Image
+                {
+                    Source = Images.Empty
+                };
+
+                images[r, c] = image;
+                GameGrid.Children.Add(image);
+            }
+        }
+
+        return images;
     }
 }
