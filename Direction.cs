20b3cce56e83419cb3f466819c9c83f9661130af
Younes-ch <younes.jmell10@gmail.com>
2025-01-01 20:27:11 +0100

    Add a class to represent the direction of the snake.

---
+++ Direction.cs
@@ -0,0 +1,44 @@
+﻿namespace SnakeGame;
+
+public class Direction
+{
+    public static readonly Direction Left = new Direction(0, -1);
+    public static readonly Direction Right = new Direction(0, 1);
+    public static readonly Direction Up = new Direction(-1, 0);
+    public static readonly Direction Down = new Direction(1, 0);
+    public int RowOffset { get; set; }
+    public int ColumnOffset { get; set; }
+
+    private Direction(int rowOffset, int columnOffset)
+    {
+        RowOffset = rowOffset;
+        ColumnOffset = columnOffset;
+    }
+
+    public Direction Opposite()
+    {
+        return new Direction(-RowOffset, -ColumnOffset);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(RowOffset, ColumnOffset);
+    }
+
+    public override bool Equals(object? other)
+    {
+        return other is Direction direction
+               && RowOffset == direction.RowOffset
+               && ColumnOffset == direction.ColumnOffset;
+    }
+
+    public static bool operator ==(Direction? left, Direction? right)
+    {
+        return EqualityComparer<Direction>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(Direction? left, Direction? right)
+    {
+        return !(left == right);
+    }
+}
